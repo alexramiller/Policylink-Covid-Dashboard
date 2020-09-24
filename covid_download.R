6c94232f6f6cd094189944e918f0fde7ece88ab0
alexramiller <alexramiller@gmail.com>
2020-09-24 16:44:52 -0700

    Added bash script

--- covid_download.R
+++ covid_download.R
@@ -2,18 +2,20 @@ suppressWarnings(suppressMessages(library(magrittr)))
 suppressWarnings(suppressMessages(library(dplyr)))
 suppressWarnings(suppressMessages(library(curl)))
 
-alameda <- read.csv(curl_download("https://opendata.arcgis.com/datasets/5d6bf4760af64db48b6d053e7569a47b_0.csv", "alameda.csv"))
-santa_clara <- read.csv(curl_download("https://data.sccgov.org/api/views/j2gj-bg6c/rows.csv?accessType=DOWNLOAD", "santa_clara.csv"))
-sf <- read.csv(curl_download("https://data.sfgov.org/api/views/tpyr-dvnc/rows.csv?accessType=DOWNLOAD", "sf.csv"))
+alameda <- read.csv(curl_download("https://opendata.arcgis.com/datasets/5d6bf4760af64db48b6d053e7569a47b_0.csv", "alameda.csv"), stringsAsFactors = FALSE)
+santa_clara <- read.csv(curl_download("https://data.sccgov.org/api/views/j2gj-bg6c/rows.csv?accessType=DOWNLOAD", "santa_clara.csv"), stringsAsFactors = FALSE)
+sf <- read.csv(curl_download("https://data.sfgov.org/api/views/tpyr-dvnc/rows.csv?accessType=DOWNLOAD", "sf.csv"), stringsAsFactors = FALSE)
 
 alameda %<>% 
+  mutate(Zip_Number = as.character(Zip_Number)) %>%
   select(ZIP = Zip_Number, Population, Cases, Rate = CaseRates)
 santa_clara %<>% 
+  mutate(zipcode = as.character(zipcode)) %>%
   select(ZIP = zipcode, Population, Cases, Rate)
 sf %<>% 
   filter(area_type == "ZCTA") %>%
-  mutate(ZIP = as.numeric(id)) %>%
-  select(ZIP, Population = acs_population, Cases = count, Rate = rate)
+  mutate(id = as.character(id)) %>%
+  select(ZIP = id, Population = acs_population, Cases = count, Rate = rate)
 
 output <- bind_rows(alameda, santa_clara, sf)
 
---
+++ covid_update.bash
@@ -0,0 +1,6 @@
+pwd
+git pull
+Rscript covid_download.R
+git add "covid_bay_counties.csv"
+git commit -m "Daily Update"
+git push
