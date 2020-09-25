a0f4e566c17269a38e4ada9d2544acc912f9602d
alexramiller <alexramiller@gmail.com>
2020-09-25 11:30:55 -0700

    Adding all bay area zipcodes

--- covid_download.R
+++ covid_download.R
@@ -2,6 +2,9 @@ suppressWarnings(suppressMessages(library(magrittr)))
 suppressWarnings(suppressMessages(library(dplyr)))
 suppressWarnings(suppressMessages(library(curl)))
 
+bay_zips <- read.csv("bayarea_zipcodes.csv", stringsAsFactors = FALSE) %>%
+  select(ZIP)
+
 alameda <- read.csv(curl_download("https://opendata.arcgis.com/datasets/5d6bf4760af64db48b6d053e7569a47b_0.csv", "alameda.csv"), stringsAsFactors = FALSE)
 santa_clara <- read.csv(curl_download("https://data.sccgov.org/api/views/j2gj-bg6c/rows.csv?accessType=DOWNLOAD", "santa_clara.csv"), stringsAsFactors = FALSE)
 sf <- read.csv(curl_download("https://data.sfgov.org/api/views/tpyr-dvnc/rows.csv?accessType=DOWNLOAD", "sf.csv"), stringsAsFactors = FALSE)
@@ -18,5 +21,6 @@ sf %<>%
   select(ZIP = id, Population = acs_population, Cases = count, Rate = rate)
 
 output <- bind_rows(alameda, santa_clara, sf)
+output <- bay_zips %>% left_join(output)
 
 write.csv(output, "covid_bay_counties.csv", row.names = FALSE)
