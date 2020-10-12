0def95bf47b562f3f6f156dd0c07bbc1687cb6ef
alexramiller <alexramiller@gmail.com>
2020-10-12 10:28:06 -0700

    Updated bash script

--- covid_update.bash
+++ covid_update.bash
@@ -1,6 +1,9 @@
 pwd
 git pull
 Rscript covid_download.R
+Rscript covid_race.R
 git add "covid_bay_counties.csv"
+git add "sf_race_new.csv"
+git add "sf_race_cumulative.csv"
 git commit -m "Daily Update"
 git push
