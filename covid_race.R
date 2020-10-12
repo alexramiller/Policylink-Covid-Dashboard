3abe79b49dd5eafd6cd14380265f03b9adea53d4
alexramiller <alexramiller@gmail.com>
2020-10-12 09:42:50 -0700

    Added SF race files for DW

--- covid_race.R
+++ covid_race.R
@@ -1,3 +1,4 @@
+library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(curl)
@@ -7,17 +8,27 @@ setwd("/Users/ajramiller/Git/Policylink-Covid-Dashboard/")
 sf <- read.csv(curl_download("https://data.sfgov.org/api/views/vqqm-nsqg/rows.csv", "sf_race.csv"), stringsAsFactors = FALSE) %>%
   mutate(Specimen.Collection.Date = as.Date(Specimen.Collection.Date))
 
-ggplot(data = sf) + geom_line(aes(x = Specimen.Collection.Date, 
-                                    y = Cumulative.Confirmed.Cases, 
-                                    group = Race.Ethnicity,
-                                    color = Race.Ethnicity))
-ggplot(data = sf) +
-  geom_point(aes(x = Specimen.Collection.Date, 
-                  y = New.Confirmed.Cases, 
-                  group = Race.Ethnicity,
-                  color = Race.Ethnicity),
-             size = 0.5, alpha = 0.5) +
-  geom_smooth(aes(x = Specimen.Collection.Date, 
-                                    y = New.Confirmed.Cases, 
-                                    group = Race.Ethnicity,
-                                    color = Race.Ethnicity))
+sf_new <- sf %>%
+  select(Specimen.Collection.Date, Race.Ethnicity, New.Confirmed.Cases) %>%
+  group_by(Specimen.Collection.Date) %>%
+  spread(Race.Ethnicity, New.Confirmed.Cases)
+
+sf_cumulative <- sf %>%
+  select(Specimen.Collection.Date, Race.Ethnicity, Cumulative.Confirmed.Cases) %>%
+  group_by(Specimen.Collection.Date) %>%
+  spread(Race.Ethnicity, Cumulative.Confirmed.Cases)
+
+#ggplot(data = sf) + geom_line(aes(x = Specimen.Collection.Date, 
+#                                    y = Cumulative.Confirmed.Cases, 
+#                                    group = Race.Ethnicity,
+#                                    color = Race.Ethnicity))
+#ggplot(data = sf) +
+#  geom_point(aes(x = Specimen.Collection.Date, 
+#                  y = New.Confirmed.Cases, 
+#                  group = Race.Ethnicity,
+#                  color = Race.Ethnicity),
+#             size = 0.5, alpha = 0.5) +
+#  geom_smooth(aes(x = Specimen.Collection.Date, 
+#                                    y = New.Confirmed.Cases, 
+#                                    group = Race.Ethnicity,
+#                                    color = Race.Ethnicity))
