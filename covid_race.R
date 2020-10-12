dffebe1f8ec8ae85ae76ff11515a4da01b661c9e
alexramiller <alexramiller@gmail.com>
2020-10-12 10:02:53 -0700

    Implemented rolling average

--- covid_race.R
+++ covid_race.R
@@ -2,6 +2,7 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(curl)
+library(zoo)
 #setwd("/Users/ajramiller/Dropbox/2020-2021/PolicyLink/COVID/")
 setwd("/Users/ajramiller/Git/Policylink-Covid-Dashboard/")
 #sf <- read.csv("COVID-19__Cases_Summarized_by_Race_and_Ethnicity.csv") %>%
@@ -11,12 +12,17 @@ sf <- read.csv(curl_download("https://data.sfgov.org/api/views/vqqm-nsqg/rows.cs
 sf_new <- sf %>%
   select(Specimen.Collection.Date, Race.Ethnicity, New.Confirmed.Cases) %>%
   group_by(Specimen.Collection.Date) %>%
-  spread(Race.Ethnicity, New.Confirmed.Cases)
+  spread(Race.Ethnicity, New.Confirmed.Cases) %>%
+  ungroup() %>%
+  mutate_at(vars(Asian:White), function(x) rollmean(x, 7))
 
 sf_cumulative <- sf %>%
   select(Specimen.Collection.Date, Race.Ethnicity, Cumulative.Confirmed.Cases) %>%
   group_by(Specimen.Collection.Date) %>%
   spread(Race.Ethnicity, Cumulative.Confirmed.Cases)
+
+write.csv(sf_new, "sf_race_new.csv", row.names = FALSE)
+write.csv(sf_cumulative, "sf_race_cumulative.csv", row.names = FALSE)
 
 #ggplot(data = sf) + geom_line(aes(x = Specimen.Collection.Date, 
 #                                    y = Cumulative.Confirmed.Cases, 
